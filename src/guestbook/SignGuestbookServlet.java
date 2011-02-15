818c0be69d79ca2d1eef9442933faed724ce77b4
Pedro Machado <pedro13522@gmail.com>
2011-02-15 00:38:39 +0000

    The last step in this example...

--- src/guestbook/SignGuestbookServlet.java
+++ src/guestbook/SignGuestbookServlet.java
@@ -1,11 +1,16 @@
 package guestbook;
 
 import java.io.IOException;
+import java.util.Date;
 import java.util.logging.Logger;
+import javax.jdo.PersistenceManager;
 import javax.servlet.http.*;
 import com.google.appengine.api.users.User;
 import com.google.appengine.api.users.UserService;
 import com.google.appengine.api.users.UserServiceFactory;
+
+import guestbook.Greeting;
+import guestbook.PMF;
 
 public class SignGuestbookServlet extends HttpServlet {
     private static final Logger log = Logger.getLogger(SignGuestbookServlet.class.getName());
@@ -16,14 +21,16 @@ public class SignGuestbookServlet extends HttpServlet {
         User user = userService.getCurrentUser();
 
         String content = req.getParameter("content");
-        if (content == null) {
-            content = "(No greeting)";
+        Date date = new Date();
+        Greeting greeting = new Greeting(user, content, date);
+
+        PersistenceManager pm = PMF.get().getPersistenceManager();
+        try {
+            pm.makePersistent(greeting);
+        } finally {
+            pm.close();
         }
-        if (user != null) {
-            log.info("Greeting posted by user " + user.getNickname() + ": " + content);
-        } else {
-            log.info("Greeting posted anonymously: " + content);
-        }
+
         resp.sendRedirect("/guestbook.jsp");
     }
 }
